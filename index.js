fd869489ceb25abdf87d759d127b3d33e22966e3
Gyatul <153093042+Gyatul@users.noreply.github.com>
2024-06-03 18:01:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
+
+// Ganti dengan token bot Telegram Anda
+const token = '7069670027:AAGrM4SX8wAyoFmZpKQQo83qS3aANewmleI';
+
+// Inisialisasi bot dengan token
+const bot = new TelegramBot(token, { polling: true });
 
 function logActivity(msg) {
   const user = msg.from;
